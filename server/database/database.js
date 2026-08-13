7e0e334cad500b1ee6a76b0abed6a6702ca5d1fa
Matheus Carvalho <matheusscarvlh@gmail.com>
2026-08-13 20:22:43 -0300

    fix: migra para postgress2

--- server/database/database.js
+++ server/database/database.js
@@ -8,6 +8,7 @@ if (!process.env.DATABASE_URL) {
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  options: "-c search_path=avaliacoes,public",
 });
 
 module.exports = pool;
